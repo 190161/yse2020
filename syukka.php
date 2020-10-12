d8ceeccc708f8750b18ebc9798af36856d0a26ef
190161 <190161xh@yse-c.net>
2020-10-12 11:36:35 +0900

    ⑩SQLの実行結果の変数から1レコードのデータを取り出すレコードがない場合はループを終了
    ⑪extract変数を使用し、1レコードのデータを渡す
    
    ⑫IDを設定
    
    ⑬IDを表示
    
    ⑭titleを表示
    
    ⑮authorを表示
    
    ⑯salesDateを表示
    
    ⑰priceを表示
    
    ⑱stockを表示

--- syukka.php
+++ syukka.php
@@ -13,25 +13,37 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①の処理を行う */) {
-	//②セッションを開始する
-}
+// if (/* ①の処理を行う */) {
+// 	//②セッションを開始する
+// }
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
-	//⑤ログイン画面へ遷移する。
-}
+// if (/* ③の処理を書く */){
+// 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+// 	//⑤ログイン画面へ遷移する。
+// }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
+$db_name = "zaiko2020_yse";
+$host = "localhost";
+$user_name = "root";
+$password = "";
+$dsn = "mysql:dbname={$db_name};host={$host}";
 
-//⑦データベースで使用する文字コードを「UTF8」にする
-
-//⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
+try{
+	$pdo = new PDO($dsn, $user_name, $password);
+	echo "接続成功1".PHP_EOL;
+}catch(PDDException $e){
+	echo "接続失敗:".$e->getMessage().PHP_EOL;
+	exit;
 }
+//⑦データベースで使用する文字コードを「UTF8」にする
+$dbh = new PDO("mysql:host=localhost;db_name=zaiko2020_yse;charset=utf8;",  $user_name,  $password );
+//⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
+// if(/* ⑧の処理を行う */){
+// 	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
+// 	//⑩在庫一覧画面へ遷移する。
+// }
 
 function getId($id,$con){
 	/* 
@@ -74,9 +86,9 @@ function getId($id,$con){
 		 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 		 * 設定されていた場合はif文の中に入る。
 		 */ 
-		if(/* ⑬の処理を書く */){
-			//⑭SESSIONの「error」の中身を表示する。
-		}
+		// if(/* ⑬の処理を書く */){
+		// 	//⑭SESSIONの「error」の中身を表示する。
+		// }
 		?>
 		</div>
 		<div id="center">
@@ -96,21 +108,23 @@ function getId($id,$con){
 				/*
 				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
 				 */
-				foreach(/* ⑮の処理を書く */){
+				$sql = "SELECT * FROM books";
+				$row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+				//foreach(/* ⑮の処理を書く */){
 					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
 				?>
-				<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
+				<!--<input type="hidden" value="<?//php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">-->
 				<tr>
-					<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
+					<td><?php echo	$row['id'].PHP_EOL; /* ⑱ ⑯の戻り値からidを取り出し、表示する */?></td>
+					<td><?php //echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
+					<td><?php //echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
 					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
 				</tr>
 				<?php
-				}
+				//}
 				?>
 			</table>
 			<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
